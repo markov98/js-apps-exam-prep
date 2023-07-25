540da309225b8711d38d49a4b8822333ca3c4796
markov98 <74935630+markov98@users.noreply.github.com>
2023-07-25 14:40:18 +0300

    added dashboard functionality

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,11 +1,36 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
+import { getAllShoes } from "../api/data.js";
 
-const template = () => html`
+const template = (shoes) => html`
         <section id="dashboard">
-          <h2>Collectibles</h2>
-          <ul class="card-wrapper">
+            <h2>Collectibles</h2>
+                ${shoes.length !== 0 ?
+                
+                html`
+                <ul class="card-wrapper">
+                    ${shoes.map(shoe => html`
+                    <li class="card">
+                    <img src="${shoe.imageUrl}" alt="travis" />
+                    <p>
+                        <strong>Brand: </strong><span class="brand">${shoe.brand}</span>
+                    </p>
+                    <p>
+                        <strong>Model: </strong
+                        ><span class="model">${shoe.model}</span>
+                    </p>
+                    <p><strong>Value:</strong><span class="value">${shoe.value}</span>$</p>
+                    <a class="details-btn" href="">Details</a>
+                    </li>
+                    `)}
+                </ul>
+                `   :
+                html`
+                <h2>There are no items added yet.</h2>`
+            }
         <section>`
 
-export function showDashboard(ctx) {
-    ctx.render(template())
+export async function showDashboard(ctx) {
+    const shoes = await getAllShoes();
+
+    ctx.render(template(shoes))
 }
