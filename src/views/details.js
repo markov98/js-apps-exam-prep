c07d36d038e4fb8936676dfc52d342362a60656e
markov98 <74935630+markov98@users.noreply.github.com>
2023-07-25 16:23:44 +0300

    updating details page

--- src/views/details.js
+++ src/views/details.js
@@ -7,16 +7,16 @@ const template = (shoe, user) => html`
           <div id="details-wrapper">
             <p id="details-title">Shoe Details</p>
             <div id="img-wrapper">
-              <img src="./images/travis.jpg" alt="example1" />
+              <img src="${shoe.imageUrl}" alt="example1" />
             </div>
             <div id="info-wrapper">
-              <p>Brand: <span id="details-brand">Air Jordan</span></p>
+              <p>Brand: <span id="details-brand">${shoe.brand}</span></p>
               <p>
-                Model: <span id="details-model">1 Retro High TRAVIS SCOTT</span>
+                Model: <span id="details-model">${shoe.model}</span>
               </p>
-              <p>Release date: <span id="details-release">2019</span></p>
-              <p>Designer: <span id="details-designer">Travis Scott</span></p>
-              <p>Value: <span id="details-value">2000</span></p>
+              <p>Release date: <span id="details-release">${shoe.release}</span></p>
+              <p>Designer: <span id="details-designer">${shoe.designer}</span></p>
+              <p>Value: <span id="details-value">${shoe.value}</span></p>
             </div>
 
             <!--Edit and Delete are only for creator-->
